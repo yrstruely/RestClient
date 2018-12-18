f3944ecf0368c9c29a044c3143ea016110e33bcd
Kerry Harris <Kerry.Harris@southerncross.co.nz>
2018-12-19 09:25:35 +1300

    Added BaseRequest.cs

---
+++ RestClient/BaseRequest.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+using System.Net;
+
+
+public class BaseRequest<T>
+{
+    public List<(string, string)> Headers { get; set; }
+    public T Body { get; set; }
+    public HttpStatusCode ExpectedResponseStatusCode { get; set; }
+}
